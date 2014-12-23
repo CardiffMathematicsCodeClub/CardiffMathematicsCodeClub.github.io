a75b509070f19f05bde8ca079c63c10c2ebfd186
vince <vincent.knight@gmail.com>
2014-12-23 11:59:05 +0000

    Have test that passes

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+browser.get('http://0.0.0.0:4000/')
 
-assert 'Cardiff School of Mathematics Code Gathering' in browser.title
+assert 'Cardiff School of Mathematics Code Club' in browser.title
