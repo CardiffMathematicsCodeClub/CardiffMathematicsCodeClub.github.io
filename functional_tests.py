f9bd114af222eb753387b8f3adfddcd4f59e8851
vince <vincent.knight@gmail.com>
2014-12-23 14:32:54 +0000

    Have test for all blog posts

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from itertools import izip
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -25,10 +26,9 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_click_on_blog(self):
         # Thomas want to read the blog page from the homepage
-        # He open the browser at the blog page
+        # He open the browser at the home page
         self.browser.get('http://0.0.0.0:4000/')
         self.browser.implicitly_wait(3)
-
 
         # He finds the link to the blog
         link = self.browser.find_element_by_link_text('blog')
@@ -45,6 +45,30 @@ class NewVisitorTest(unittest.TestCase):
         # He then checks that the correct header is in place
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Code Club Blog', header_text)
+
+    def test_that_can_click_on_all_blog_posts(self):
+        # Pauline wants to read every blog post
+        # She open the browser at the blog page
+        self.browser.get('http://0.0.0.0:4000/blog/')
+        self.browser.implicitly_wait(3)
+
+        # She finds all links to blog post
+        links = self.browser.find_elements_by_class_name('post-link')
+
+        # She reads all the titles
+        titles = [link.text for link in links]
+
+        # She then clicks on every link and checks that the title is correct and in the header
+        for link, title in izip(links, titles):
+            # She clicks on the link
+            link.click()
+            self.browser.implicitly_wait(3)
+
+            # Checks the browser title
+            self.assertEqual(title, self.browser.title)
+            # Reads the header and checks that it is correct
+            header_text = self.browser.find_element_by_tag_name('h1').text
+            self.assertEqual(title, header_text)
 
 if __name__ == '__main__':
     #unittest.main(warnings='ignore')
