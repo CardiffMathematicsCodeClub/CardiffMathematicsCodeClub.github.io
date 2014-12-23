2eed4b1e58dbe7130eae658acdb2258fa45fae60
vince <vincent.knight@gmail.com>
2014-12-23 13:57:12 +0000

    Have functional tests with a failure - have identified something that should not be there

--- functional_tests.py
+++ functional_tests.py
@@ -10,13 +10,38 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_check_the_blog(self):
+    def test_can_read_the_home_page(self):
         # Zoe wants to take a look at the code club site.
         # She opens up the browser at the homepage:
         self.browser.get('http://0.0.0.0:4000/')
+        self.browser.implicitly_wait(3)
 
         # She checks that the title shows that she's in the correct place.
-        assert 'Cardiff School of Mathematics Code Club' in self.browser.title
+        self.assertIn('Cardiff School of Mathematics Code Club', self.browser.title)
+
+        # She checks that the header also says Code club:
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Cardiff School of Mathematics Code Club', header_text)
+
+    def test_can_click_on_blog(self):
+        # Thomas want to read the blog page from the homepage
+        # Thomas open the browser at the blog page
+        self.browser.get('http://0.0.0.0:4000/')
+        self.browser.implicitly_wait(3)
+
+
+        # Thomas finds the link to the blog
+        link = self.browser.find_element_by_link_text('blog')
+        # Thomas clicks on the link
+        link.click()
+        self.browser.implicitly_wait(3)
+
+        # Thomas checks that the correct url is located
+        self.assertEquals('http://0.0.0.0:4000/blog/', self.browser.current_url)
+
+        # Thomas then checks that the correct header is in place
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Posts', header_text)
 
 if __name__ == '__main__':
     #unittest.main(warnings='ignore')
