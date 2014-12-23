6e29b8f94d0745eac437654c4ef29c7c1054c8f8
vince <vincent.knight@gmail.com>
2014-12-23 15:05:41 +0000

    Have test for past sessions

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from itertools import izip
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class VisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -10,6 +10,9 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+
+class NewVisitorTest(VisitorTest):
 
     def test_can_read_the_home_page(self):
         # Zoe wants to take a look at the code club site.
@@ -23,6 +26,9 @@ class NewVisitorTest(unittest.TestCase):
         # She checks that the header also says Code club:
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Cardiff School of Mathematics Code Club', header_text)
+
+
+class BlogReaderTest(VisitorTest):
 
     def test_can_click_on_blog(self):
         # Thomas want to read the blog page from the homepage
@@ -52,23 +58,56 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://0.0.0.0:4000/blog/')
         self.browser.implicitly_wait(3)
 
-        # She finds all links to blog post
+        # She finds all links
         links = self.browser.find_elements_by_class_name('post-link')
+
+        # She finds all urls to blog post
+        urls = [link.get_attribute("href") for link in links]
 
         # She reads all the titles
         titles = [link.text for link in links]
 
         # She then clicks on every link and checks that the title is correct and in the header
-        for link, title in izip(links, titles):
-            # She clicks on the link
-            link.click()
+        for url, title in izip(urls, titles):
+            # She goes to the url
+            self.browser.get(url)
             self.browser.implicitly_wait(3)
 
-            # Checks the browser title
+            # She checks the browser title
             self.assertEqual(title, self.browser.title)
-            # Reads the header and checks that it is correct
+            # She reads the header and checks that it is correct
             header_text = self.browser.find_element_by_tag_name('h1').text
             self.assertEqual(title, header_text)
+
+            # She click on the Blog header to return to list of blog posts
+
+            link = self.browser.find_element_by_link_text('Blog')
+            link.click()
+
+
+class PastSessionsReaderTest(VisitorTest):
+
+    def test_can_click_on_past_sessions(self):
+        # Julien wants to take a look at the past sessions page
+        # He opens up the browser at the homepage:
+        self.browser.get('http://0.0.0.0:4000/')
+        self.browser.implicitly_wait(3)
+
+        # He finds the link to the past sessions page
+        link = self.browser.find_element_by_link_text('Past Sessions')
+        # He clicks on the link
+        link.click()
+        self.browser.implicitly_wait(3)
+
+        # He checks that the correct url is located
+        self.assertEquals('http://0.0.0.0:4000/sessions.html', self.browser.current_url)
+
+        # He checks that the title shows that he's in the correct place
+        self.assertIn('Past Sessions', self.browser.title)
+
+        # He then checks that the correct header is in place
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Past Sessions', header_text)
 
 if __name__ == '__main__':
     #unittest.main(warnings='ignore')
